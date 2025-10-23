27b71cf60409b5c14f329fe4693687f304883a01
Sergio Zavala <sezavala@csumb.edu>
2025-10-23 13:04:57 -0700

    spring project

---
+++ src/main/java/com/gaelcraves/project3/GaelCravings_Backend/GaelCravingsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gaelcraves.project3.GaelCravings_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaelCravingsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaelCravingsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaelcraves/project3/GaelCravings_Backend/GaelCravingsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaelcraves.project3.GaelCravings_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaelCravingsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
